73088b65fec956d4f6dfb9f80f67989a020c8681
Marcus-Philipp <philippmarcus89@gmail.com>
2023-10-04 10:15:54 +0200

    tailwind purge und safelist hinzugefuegt/ Cell UI

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import Mine from '../mine-icon.png';
+import Flag from '../flag-icon.png';
 
 const Cell = ({ data, rowIndex, cellIndex }) => {
+
+     const displayContent = (content) => {
+
+        const colors = {
+            1: 'blue',
+            2: 'green',
+            3: 'red',
+            4: 'pink',
+            5: 'yellow',
+            6: 'violet',
+            7: 'gray',
+            8: 'purple'
+        };
+
+        if (content === 'M') {
+            return <img src={Mine} alt='Mine'/>;
+        } else if (typeof content === 'number') {
+            return <span className={`text-${colors[content]}-500 font-bold`}>{content}</span>;
+        } else {
+            return content;
+        }
+     };
     
     return (
         <div className="flex items-center justify-center cursor-default bg-gray-300 border h-6 w-6">
-            {data}
+            {displayContent(data)}
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+  purge: {
+    content: ["./src/**/*.{html,js,jsx}"],
+    options: {
+      safelist: [
+        'text-blue-500', 
+        'text-green-500', 
+        'text-red-500', 
+        'text-teal-500',
+        'text-yellow-500', 
+        'text-violet-500', 
+        'text-gray-500', 
+        'text-pink-500'
+      ],
+    },
+  },
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'mine-pattern': "url('../mine-icon.png')",
+        'flag-pattern': "url('../flag-icon.png')"
+      }
+    },
   },
   plugins: [],
 }
