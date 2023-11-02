f5a1eadb57a92a7e955a41f5e60ac1bad5ed5129
Marcus-Philipp <philippmarcus89@gmail.com>
2023-11-02 22:08:19 +0100

    GameBoard stylischer gemacht

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -6,34 +6,43 @@ import MineCounter from "./MineCounter";
 import Withdrawal from "./Withdrawal";
 import ExposedMines from "./ExposedMines";
 import PlayButton from "./PlayButton";
+import MinesweeperLogo from "../Minesweeper-logo.png";
 
 const GameBoard = () => {
   //Holt sich das Spielfeld aus dem Kontext
   const { gameField } = useContext(GameContext);
 
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen bg-green-50">
-      <ControlPanel />
-      <PlayButton />
+    <div className="flex justify-center items-center">
       <div>
-        <div className="flex flex-col items-center justify-between lg:flex-row">
-          <MineCounter />
-          <ExposedMines />
-        </div>
-        {gameField.map((row, rowIndex) => (
-          <div className="flex" key={rowIndex}>
-            {row.map((cellData, cellIndex) => (
-              <Cell
-                key={cellIndex}
-                cellValue={cellData}
-                rowIndex={rowIndex}
-                cellIndex={cellIndex}
-              />
-            ))}
-          </div>
-        ))}
+        <img src={MinesweeperLogo} alt="Minesweeper-Logo" />
       </div>
-      <Withdrawal />
+      <div className="flex flex-col justify-center items-center min-h-screen bg-green-50">
+        <ControlPanel />
+        <PlayButton />
+        <div>
+          <div className="flex flex-col items-center justify-between lg:flex-row">
+            <MineCounter />
+            <ExposedMines />
+          </div>
+          {gameField.map((row, rowIndex) => (
+            <div className="flex" key={rowIndex}>
+              {row.map((cellData, cellIndex) => (
+                <Cell
+                  key={cellIndex}
+                  cellValue={cellData}
+                  rowIndex={rowIndex}
+                  cellIndex={cellIndex}
+                />
+              ))}
+            </div>
+          ))}
+        </div>
+        <Withdrawal />
+      </div>
+      <div>
+        <img src={MinesweeperLogo} alt="Minesweeper-Logo" />
+      </div>
     </div>
   );
 };
