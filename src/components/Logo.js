95817722412bea8e317687fb6b23976bcac8ce2b
Marcus-Philipp <philippmarcus89@gmail.com>
2023-11-03 14:14:16 +0100

    anpassung mobile version

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -6,7 +6,7 @@ const Logo = () => {
   const isMobile = useMediaQuery({ query: "(max-width: 500px)" });
 
   return isMobile ? (
-    <div className="flex w-10 justify-center pb-5">
+    <div className="flex w-12 justify-center pb-5">
       <img src={MinesweeperLogo} alt="Minesweeper-Logo" />
       <h1 className="font-bold text-lg bg-gradient-to-r from-red-500 via-red-300 to-red-500 rounded">MINESWEEPER</h1>
       <img src={MinesweeperLogo} alt="Minesweeper-Logo" />
